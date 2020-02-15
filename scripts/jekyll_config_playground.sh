82ff18fdb5bd1f5e40c6eae5b0032b3efe0b76f7
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-02-15 16:52:12 +0100

    Add depencies list and .gitignore

--- scripts/jekyll_config_playground.sh
+++ scripts/jekyll_config_playground.sh
@@ -16,3 +16,37 @@ echo $GEM_HOME
 
 # install Jekyll
 gem install jekyll bundler
+
+# create a branch for introduction of Jekyll
+git checkout -b feature-jekyll master 
+git push origin feature-jekyll
+
+# create a Gemfile list project dependencies
+bundle init
+
+# add Jekyll as a dependency
+echo 'gem "jekyll"' >> Gemfile
+
+# install Jekyll for current project
+bundle
+
+# Make sure to use the Jekyll version defined in Gemfile
+#  -> prefix all further Jekyll commands with 'bundle exec'
+
+# add .gitignore file tailored for Jekyll
+cat <<-GITIGNORE > .gitignore
+	# volatile folders
+	.tmp/*
+
+	# jekyll
+	_site/
+	.sass-cache/
+	.jekyll-cache/
+	.jekyll-metadata
+
+	# github pages
+	Gemfile.lock
+GITIGNORE
+
+# # track these files, if they exist
+# !.gitignore
